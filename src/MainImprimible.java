e0436b102c89107650c2f6da326d0afe10ae215e
Judith-olmand <judith.olmand@educa.jcyl.es>
2025-12-01 17:23:00 +0100

    comentarios añadidos

--- src/MainImprimible.java
+++ src/MainImprimible.java
@@ -6,6 +6,15 @@ public class MainImprimible {
 
         libro1.primerasLineas();
         libro1.imprimir();
+        /**
+         * libro.primerasLineas(); esto no se puede porque libro aunque es objero de Libro
+         * referencia a Imprimible.
+         * Puedes llamar solo a los métodos definidos en Imprimible
+         * El objeto real tiene todas las características de Libro
+         * Si necesitas acceder a métodos específicos de Libro, necesitarías hacer un casting:
+         * ((Libro)libro).metodoEspecificoDeLibro();
+         */
+        ((Libro)libro).primerasLineas();
         System.out.println(libro1.getTitulo() + " " + libro1.getAutor());
 
         libro.imprimir();
